1ec55e28aabaf8352e4aed51a4a1888301761170
rithishreddy17 <samarithishreddy2005@gmail.com>
2025-08-18 00:47:21 +0530

    homepageFun

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -30,3 +30,26 @@ export const logout = async () => {
     const res = await axiosInstance.post("/auth/logout");
     return res.data;
 };
+
+
+export const getUserFriends = async () => {
+    const res = await axiosInstance.get("/users/friends");
+    return res.data;
+};
+
+export const getRecommendedUsers = async () => {
+    const res = await axiosInstance.get("/users");
+    return res.data;
+};
+
+export const getOutgoingFriendReqs = async () => {
+    const res = await axiosInstance.get("/users/outgoing-friend-requests");
+    return res.data;
+};
+
+
+export const sendFriendRequest = async (userId) => {
+    const res = await axiosInstance.get(`/users/friend-request/${userId}`);
+    return res.data;
+};
+
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,36 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { getOutgoingFriendReqs, getRecommendedUsers, getUserFriends, sendFriendRequest } from "../lib/api";
 
 const HomePage = () => {
   
+  const queryClient = useQueryClient();
+  const [outgoingRequestsIds, setOutgoingRequestsIds] = useState([]);
+
+  const {data: friends = [], isLoading: loadingFriends} = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends,
+  });
+
+  const {data: recommendedUsers = [], isLoading: loadingUsers} = useQuery({
+    queryKey: ["users"],
+    queryFn: getRecommendedUsers,
+  });
+
+  const {data: outgoingFriendReqs} = useQuery({
+    queryKey: ["outgoingFriendReqs"],
+    queryFn: getOutgoingFriendReqs,
+  });
+
+  const {mutate: sendRequestsMutation, isPending} = useMutation({
+    mutationFn: sendFriendRequest,
+    onSuccess: () => queryClient.invalidateQueries({queryKey: ["outgoingFriendReqs"]})
+  });
+
+
   return (
     <div>
-      HomePage
+      
     </div>
   )
 }
