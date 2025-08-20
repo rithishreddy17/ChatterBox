03b47c1c3a1e18d5cdd261d13963aa7d72e29373
rithishreddy17 <samarithishreddy2005@gmail.com>
2025-08-20 18:28:26 +0530

    setting call page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -64,7 +64,14 @@ const App = () => {
           <Navigate to={!isAuthenticated ? "/login" : "/onboarding"}/>
         )
         }/>
-        <Route path="/call" element={isAuthenticated ? <CallPage/> : <Navigate to="/login"/>}/>
+        <Route path="/call/:id" element={isAuthenticated && isOnboarded? (
+          // <Layout showSidebar={false}>
+            <CallPage />
+          // </Layout>
+        ) : (
+          <Navigate to={!isAuthenticated ? "/login" : "/onboarding"}/>
+        )
+        }/>
         <Route path="/chat/:id" element={isAuthenticated && isOnboarded? (
           <Layout showSidebar={false}>
             <ChatPage />
