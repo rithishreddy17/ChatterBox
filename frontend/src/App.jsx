97088bf41eccb7bf496e7a574cf9f5805549b835
rithishreddy17 <samarithishreddy2005@gmail.com>
2025-08-20 15:46:06 +0530

    Route setup Chat page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -65,7 +65,14 @@ const App = () => {
         )
         }/>
         <Route path="/call" element={isAuthenticated ? <CallPage/> : <Navigate to="/login"/>}/>
-        <Route path="/chat" element={isAuthenticated ? <ChatPage/> : <Navigate to="/login"/>}/>
+        <Route path="/chat/:id" element={isAuthenticated && isOnboarded? (
+          <Layout showSidebar={false}>
+            <ChatPage />
+          </Layout>
+        ) : (
+          <Navigate to={!isAuthenticated ? "/login" : "/onboarding"}/>
+        )
+        }/>
         <Route path="/onboarding" 
         element={
           isAuthenticated ? (
